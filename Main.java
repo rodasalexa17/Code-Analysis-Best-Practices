940ac3c4e5edc8e4fe5df7ff66ea09e55e924ebc
rodasalexa17 <30929316+rodasalexa17@users.noreply.github.com>
2017-09-06 18:34:11 -0400

    CodeAnalysis
    Best Practices

---
+++ Main.java
@@ -0,0 +1,34 @@
+package nurseryRhymes;
+import java.io.*;
+import nurseryRhymes.oldMacdonald.*;
+
+
+public class Main {
+      
+       public static void main(String[] args) {
+              InputStreamReader input = new InputStreamReader(System.in);
+              BufferedReader newInput = new BufferedReader(input);
+ 
+              Song.songIntro();
+             
+              do {
+                     pause(newInput);
+              } while (!Song.hasAnimal);
+             
+       }
+
+	private static void pause(BufferedReader newInput) {
+		try {
+		       String animal = newInput.readLine().toLowerCase();
+		       new Song(animal);
+		 } catch (IOException e) {
+		       // TODO Auto-generated catch block
+		       e.printStackTrace();
+		 }
+	}
+             
+}
+ 
+
+ 
+
